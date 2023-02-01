6f2b822c3b770be3e9cf83aec78cd283ecd55101
Amaal Al Otaibi <hopes.gmo@gmail.com>
2023-02-01 12:50:29 +0300

    Initial Commit

---
+++ Overseas/OverseasApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverseasApp.swift
+//  Overseas
+//
+//  Created by Amaal on 10/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct OverseasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

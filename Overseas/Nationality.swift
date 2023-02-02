e543ac020bb094be1b57f92f1bf000dc09ded71b
Amaal Al Otaibi <hopes.gmo@gmail.com>
2023-02-02 16:58:47 +0300

    changes in the Nationality

--- Overseas/Nationality.swift
+++ Overseas/Nationality.swift
@@ -14,14 +14,13 @@ struct Nationality: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe.asia.australia.fill").resizable()
-                .scaledToFit()
-                .frame(width: 200).padding().foregroundColor(Color("darkBlue"))
-            
+            Image(systemName: "globe.asia.australia.fill").resizable().aspectRatio(contentMode: .fit)
+                .frame(width: 250).padding().padding(.top).foregroundColor(Color("darkBlue"))
             
             Text("Pick your Nationality")
-                .font(.title2).fontWeight(.semibold).frame(maxWidth: 330,  alignment: .leading).padding(.top)
+                .font(.system(size: 38)).frame(maxWidth: 300, alignment: .leading).padding(.top)
             
+            Divider().frame(width: 340)
             Picker("Pick your Nationality", selection: $selectedItem) { // 3
                         ForEach(options, id: \.self) { item in // 4
                             Text(item) // 5

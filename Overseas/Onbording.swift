47eda18e1f0630e8da9771603b74b130a4c2f71d
DoaaNwe <duo_nwe@hotmail.com>
2023-02-02 17:53:48 +0300

    onbording 2

--- Overseas/Onbording.swift
+++ Overseas/Onbording.swift
@@ -22,8 +22,9 @@ struct Onbording: View {
                 
                 VStack(){
                     
-                    Image("FriendsOn")
-                        .resizable().aspectRatio(contentMode: .fit)
+                    Image("onbording1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                         .frame(width: 250)
                         .padding(.top)
                     
@@ -31,6 +32,7 @@ struct Onbording: View {
                         .font(.system(size: 46))
                         .foregroundColor(Color("darkBlue"))
                         .frame(maxWidth: 300, alignment: .leading)
+                   
                     Text("Overseas")
                         .font(.system(size: 46))
                         .foregroundColor(Color("Yellow"))
@@ -38,23 +40,24 @@ struct Onbording: View {
                         .frame(maxWidth: 300, alignment: .leading)
                         .padding(.bottom)
                     
-                    Text("We Take Care Of You, Helping You To Reach Out SOS Services No Matter Where You Are")
+                    Text("Overseas assist you in reaching Authorities and Emergency services, everywhere")
                         .font(.system(size: 20))
                         .foregroundColor(Color("ourGray"))
                         .frame(maxWidth: 300, alignment: .leading)
-                    Spacer()
+                   // Spacer()
                 } .tag(1)
                 
                 
                 
-                VStack{
-                    Image("")
+                VStack (){
+                    Spacer()
+                   Image(systemName: "person.text.rectangle")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 300)
+                        .foregroundColor(Color("darkBlue"))
+                        .frame(width: 200)
                         .padding()
-                    
-                    
+                        
                     
                     Text("Please Select")
                         .font(.system(size: 32))
@@ -65,31 +68,26 @@ struct Onbording: View {
                         .font(.system(size: 24))
                         .fontWeight(.bold)
                         .foregroundColor(Color("darkBlue"))
-                        .frame(maxWidth: 300, alignment: .leading).padding(.bottom)
-                    
-                    
-//                    Picker("Please choose a color", selection: $selectedNationality) {
-//                        ForEach(nationality, id: \.self) {
-//                            Text($0)
+                        .frame(maxWidth: 300, alignment: .leading)
+                        .padding(.bottom)
                     
                        
                 PickerView()
                     
+                    
                     Button(action: {
                         //shouldShow = false
-                    },
-                           
-                           label:{Text("Done")
+                    },label:{
+                    
+                        Image(systemName: "arrowshape.turn.up.right.fill")
+                            .foregroundColor(Color("Yellow"))
                             .padding()
                             .frame(maxWidth: 180)
                             .font(.callout)
-                            .foregroundColor(.white).background(Color("Prime"))
+                            .background(Color("Prime"))
                         .cornerRadius(12)})
-                    
-                    Spacer()
-                    
-                    
-                    
+                    //.onAppear()
+                   Spacer()
                     
                 }.tag(2)
                 
